59f903f582f5fbdc3ea77b337f0bcf6cb7a364bc
Sevinc <sevincqiyasova16@gmail.com>
2025-10-31 12:20:07 +0000

    Completed

--- Scripts/Script_1.sql
+++ Scripts/Script_1.sql
@@ -2,7 +2,7 @@ WITH cte AS (SELECT
     pizza_category, 
     pizza_name, 
     sum (total_price) AS sum_price, 
-    dense_rank() OVER (PARTITION BY pizza_category ORDER BY sum (total_price)) AS dense_rnk
+    dense_rank() OVER (PARTITION BY pizza_category ORDER BY sum (total_price) DESC) AS dense_rnk
 FROM public.pizza_sales
 GROUP BY pizza_category, pizza_name)
 
